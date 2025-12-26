c56c5dc311b2ebcb601aaddd1e4a32b97bae931c
Generasikreasi <generasikrasi@gmail.com>
2025-12-26 15:29:39 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,93 @@
+import os
+from datetime import datetime
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters
+)
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SPREADSHEET_ID = os.environ.get("https://docs.google.com/spreadsheets/d/1KP1q0CT1JudlYfM-CE1C7ue99QDWBJT5L_7ybIQleEc/edit?gid=842381517#gid=842381517")
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "credential.json",
+    scope
+)
+
+client = gspread.authorize(creds)
+sheet = client.open_by_key(SPREADSHEET_ID).worksheet("Pengeluaran")
+
+def start(update, context):
+    update.message.reply_text(
+        "👋 Selamat datang!\n\n"
+        "Gunakan format:\n"
+        "Kategori | Nominal | Deskripsi\n\n"
+        "Contoh:\n"
+        "Makan & Minum | 35000 | Warteg Bu Sari"
+    )
+def handle_message(update, context):
+    text = update.message.text
+
+    if "|" not in text:
+        update.message.reply_text(
+            "❌ Format salah\n\n"
+            "Gunakan:\n"
+            "Kategori | Nominal | Deskripsi"
+        )
+        return
+
+    try:
+        kategori, nominal, deskripsi = [x.strip() for x in text.split("|", 2)]
+        nominal = int(nominal)
+
+        now = datetime.now()
+
+        row = [
+            now.strftime("%Y%m%d%H%M%S"),     # ID
+            now.strftime("%Y-%m-%d"),         # Tanggal
+            now.strftime("%H:%M:%S"),         # Waktu
+            update.message.from_user.id,      # User_ID
+            update.message.from_user.full_name,  # User_Nama
+            kategori,                         # Kategori
+            nominal,                          # Nominal
+            deskripsi,                        # Deskripsi
+            "Chat",                           # Sumber
+            "Valid",                          # Status
+            ""                                # Bukti
+        ]
+
+        sheet.append_row(row)
+
+        update.message.reply_text(
+            f"✅ Tersimpan\n\n"
+            f"Kategori : {kategori}\n"
+            f"Nominal  : Rp{nominal:,}\n"
+            f"Catatan  : {deskripsi}"
+        )
+
+    except Exception:
+        update.message.reply_text("❌ Gagal menyimpan data")
+
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
+
